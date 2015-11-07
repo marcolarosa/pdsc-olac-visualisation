709f81fabcbcb0eea13fa83ced28745c95c6fb70
Marco La Rosa <m@lr.id.au>
2015-11-07 16:23:56 +1100

    build region and country data structures

--- process-language-pages/create-summary.py
+++ process-language-pages/create-summary.py
@@ -4,7 +4,96 @@ import argparse
 import logging
 import os
 import os.path
+from lxml import html, etree
 import json
+import sys
+
+class CreateSummary:
+    def __init__(self, languages):
+        self.languages = languages
+
+    def process(self):
+        # delete previous summaries if any
+        for summary in [ 'index.json', 'regions.json', 'countries.json' ]:
+            summary_file = os.path.join(self.languages, summary)
+            if os.path.exists(summary_file):
+                os.remove(summary_file)
+
+        summary = []
+        for root, dirs, files in os.walk(self.languages):
+            for f in files:
+                log.info("Processing: %s" % os.path.join(f));
+
+                with open(os.path.join(root, f), 'r') as f:
+                    data = json.loads(f.read())
+                    s = {
+                        'name': data['name'],
+                        'url': data['url'],
+                        'coords': data['coords'],
+                    }
+                    r = {}
+                    for resource in data['resources']:
+                        r[resource] = data['resources'][resource]['count']
+                    s['resources'] = r
+                    summary.append(s)
+
+        with open(summary_file, 'w') as f:
+            f.write(json.dumps(summary))
+
+
+class CreateRegionSummaries:
+    def __init__(self, languages):
+        self.languages = languages
+        self.region_pages = [
+            'http://www.language-archives.org/area/africa',
+            'http://www.language-archives.org/area/americas',
+            'http://www.language-archives.org/area/asia',
+            'http://www.language-archives.org/area/europe',
+            'http://www.language-archives.org/area/pacific'
+        ]
+
+    def process(self):
+        regions = {}
+        for region in self.region_pages:
+            log.info("Processing region: %s" % region)
+            base_url = region.split('area')[0].rstrip('/')
+
+            region_data = []
+
+            tree = html.parse(region)
+            for e in tree.findall('//table[2]/tr/td/ul/li'):
+                region_data.append({
+                    'name': e.text_content().split('(')[0].strip(),
+                    'count': e.text_content().split('(')[1].split(')')[0],
+                    'url': "%s%s" % (base_url, e.xpath('a/@href')[0])
+                })
+
+            regions[region.split('/')[-1:][0]] = region_data
+
+        with open(os.path.join(self.languages, 'regions.json'), 'w') as f:
+            f.write(json.dumps(regions))
+
+        countries = {}
+        for region in regions:
+            for country in regions[region]:
+                log.info("Processing country: %s" % country['name'])
+                base_url = country['url'].split('country')[0].rstrip('/')
+                tree = html.parse(country['url'])
+
+                language_data = []
+
+                for e in tree.findall('//table[2]/tr/td/ul/li'):
+                    language_data.append({
+                        'name': e.text_content().split('(')[0].strip(),
+                        'url': "%s%s" % (base_url, e.xpath('a/@href')[0]),
+                        'code': e.xpath('a/@href')[0].split('/')[2]
+                    })
+
+                country['language_data'] = language_data
+                countries[country['name']] = country
+
+        with open(os.path.join(self.languages, 'countries.json'), 'w') as f:
+            f.write(json.dumps(countries))
 
 if __name__ == "__main__":
 
@@ -32,31 +121,10 @@ if __name__ == "__main__":
     # get the logger
     log = logging.getLogger('LANGUAGE_PROCESSOR')
 
-    # delete previous summary if exists
-    summary_file = os.path.join(args.languages, "index.json")
-    if os.path.exists(summary_file):
-        os.remove(summary_file)
-
-    summary = []
-    for root, dirs, files in os.walk(args.languages):
-        for f in files:
-            log.info("Processing: %s" % os.path.join(f));
-
-            with open(os.path.join(root, f), 'r') as f:
-                data = json.loads(f.read())
-                s = {
-                    'name': data['name'],
-                    'url': data['url'],
-                    'coords': data['coords'],
-                }
-                r = {}
-                for resource in data['resources']:
-                    r[resource] = data['resources'][resource]['count']
-                s['resources'] = r
-                summary.append(s)
-
-    with open(summary_file, 'w') as f:
-        f.write(json.dumps(summary))
-
-
- 
+    # create summary file
+    s = CreateSummary(args.languages)
+    s.process()
+    
+    # create region / country summaries
+    s = CreateRegionSummaries(args.languages)
+    s.process()
