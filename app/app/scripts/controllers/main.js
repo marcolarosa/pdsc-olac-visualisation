3d8efca691b5be6f26f1c6a89e414a516f70ce58
Marco La Rosa <m@lr.id.au>
2015-11-28 17:46:33 +1100

    working on country / language selection

--- app/app/scripts/controllers/main.js
+++ app/app/scripts/controllers/main.js
@@ -9,17 +9,44 @@
  */
 angular.module('appApp')
   .controller('MainCtrl', [ '$scope', '$http', function ($scope, $http) {
+      $scope.$watch('selected', function() {
+          if ($scope.selected) console.log($scope.selected);
+      }, true);
+      $scope.selected = {};
+      $scope.datasets = {
+          'languages': undefined,
+          'countries': undefined,
+          'regions': undefined
+      }
+
+      var search = function(what, item) {
+          var searchWhat = $scope.datasets[what];
+          if (item.name !== '') {
+              return _.filter(searchWhat, function(whats) { return whats.name.match(item.name); });
+          } else {
+              return searchWhat;
+          }
+      }
+
+      $scope.searchLanguages = function(item) {
+          return search('languages', item);
+      };
+      $scope.searchCountries = function(item) {
+          return search('countries', item);
+      };
+
+
       $http.get('/data/index.json').then(function(resp) {
           console.log('Languages', resp.data);
-          $scope.languages = _.groupBy(resp.data, 'name');
+          $scope.datasets.languages = resp.data;
       });
       $http.get('/data/regions.json').then(function(resp) {
           console.log('Regions', resp.data);
-          $scope.regions = resp.data;
+          $scope.datasets.regions = resp.data;
       });
       $http.get('/data/countries.json').then(function(resp) {
           console.log('Countries', resp.data);
-          $scope.countries = resp.data;
+          $scope.datasets.countries = resp.data;
       });
 
   }]);
