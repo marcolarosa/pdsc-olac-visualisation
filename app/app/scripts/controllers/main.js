f9a276ffab419ee83e752985ce71ca83b661bcfc
Marco La Rosa <m@lr.id.au>
2015-11-29 21:35:20 +1100

    bugfix autocomplete and playing with markers

--- app/app/scripts/controllers/main.js
+++ app/app/scripts/controllers/main.js
@@ -22,7 +22,9 @@ angular.module('appApp')
       var search = function(what, item) {
           var searchWhat = $scope.datasets[what];
           if (item.name !== '') {
-              return _.filter(searchWhat, function(whats) { return whats.name.match(item.name); });
+              return _.filter(searchWhat, function(whats) { 
+                  return whats.name.toLowerCase().match(item.name.toLowerCase()); 
+              });
           } else {
               return searchWhat;
           }
@@ -35,10 +37,29 @@ angular.module('appApp')
           return search('countries', item);
       };
 
-
       $http.get('/data/index.json').then(function(resp) {
           console.log('Languages', resp.data);
           $scope.datasets.languages = resp.data;
+
+          var markers = _.map($scope.datasets.languages, function(l) {
+              try {
+                  if (parseFloat(l.coords[0])  && parseFloat(l.coords[2])) {
+                      return { 
+                          lat: parseFloat(l.coords[0]),
+                          lng: parseFloat(l.coords[2]),
+                          message: l.name
+                      }
+                  }
+              } catch (e) {
+                  // do nothing
+              }
+          });
+          $scope.markers = {};
+          markers = _.groupBy(_.compact(markers).slice(0,500), 'message');
+          _.each(markers, function(m, i) {
+              $scope.markers[_.camelCase(i)] = m[0];
+          });
+          console.log($scope.markers);
       });
       $http.get('/data/regions.json').then(function(resp) {
           console.log('Regions', resp.data);
