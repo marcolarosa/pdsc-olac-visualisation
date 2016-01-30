a189c58cf3f8dfd64fc7d1820e3447b7e8349c74
Marco La Rosa <m@lr.id.au>
2016-01-30 17:16:34 +1100

    configure the bits extra testing bits required

--- app/test/karma.conf.js
+++ app/test/karma.conf.js
@@ -38,7 +38,8 @@ module.exports = function(config) {
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
-      "test/spec/**/*.js"
+      "test/spec/**/*.js",
+      "app/views/*.html"
     ],
 
     // list of files / patterns to exclude
@@ -63,8 +64,17 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       "karma-phantomjs-launcher",
-      "karma-jasmine"
+      "karma-jasmine",
+      "karma-ng-html2js-preprocessor"
     ],
+
+    preprocessors: {
+        'app/views/*.html':['ng-html2js']
+    },
+    ngHtml2JsPreprocessor: { 
+        stripPrefix: 'app/', 
+        moduleName: 'my.templates'
+    },
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
